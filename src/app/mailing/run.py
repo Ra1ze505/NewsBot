c9875eb98d25c73822ee9aaf3e12db2037efcdcc
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-05-21 15:58:27 +0300

    refactoring

--- src/app/mailing/run.py
+++ src/app/mailing/run.py
@@ -6,18 +6,18 @@ from app.mailing.service import get_all_users, get_day_news
 from config.settings import BOT_TOKEN, API_ID, API_HASH
 
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+class Mailing:
+    def __init__(self):
+        self.client = TelegramClient('mailing', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
+    async def start_mailing(self):
+        users = await get_all_users()
+        message = await get_day_news()
+        await asyncio.gather(*[self.mailing(user.chat_id, message) for user in users])
 
-async def mailing(user_id, message):
-    await bot.send_message(user_id, message.text)
-
-
-async def start_mailing():
-    users = await get_all_users()
-    message = await get_day_news()
-    await asyncio.gather(*[mailing(user.chat_id, message) for user in users])
+    async def mailing(self, user_id, message):
+        await self.client.send_message(user_id, message.text)
 
 
 if __name__ == '__main__':
-    bot.loop.run_until_complete(start_mailing())
+    asyncio.run(Mailing().start_mailing())
--- src/app/parser/run.py
+++ src/app/parser/run.py
@@ -4,14 +4,12 @@ from app.parser.service import create_news
 from config.settings import API_ID, API_HASH, NEWS_CHANEL, KEY_WORD
 
 
-async def parse_last_news():
-    chat = await parser.get_entity(NEWS_CHANEL)
-    async for message in parser.iter_messages(chat, search=KEY_WORD):
-        await create_news(message)
-        break
+class Parser:
+    def __init__(self):
+        self.client = TelegramClient('parser', API_ID, API_HASH).start()
 
-
-if __name__ == '__main__':
-    parser = TelegramClient('parser', API_ID, API_HASH)
-    parser.start()
-    parser.loop.run_until_complete(parse_last_news())
+    async def parse_last_news(self):
+        chat = await self.client.get_entity(NEWS_CHANEL)
+        async for message in self.client.iter_messages(chat, search=KEY_WORD):
+            await create_news(message)
+            break
