2d311b99a0ad376cd0204083b9bb610b6526951c
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-05-22 19:51:53 +0300

    add new setting

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,6 +5,7 @@ import os
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 API_ID = os.environ.get('API_ID')
 API_HASH = os.environ.get('API_HASH')
+STRING_SESSION = os.environ.get('STRING_SESSION')
 
 # Database
 DB_DRIVER = os.environ.get('DB_DRIVER', 'sqlite')
