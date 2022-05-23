711280691527502d250ba4bd955616ea3f755fd0
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-05-23 16:35:48 +0300

    add currency rate in mailing

--- src/app/parser/service.py
+++ src/app/parser/service.py
@@ -1,28 +1,32 @@
+import json
 import logging
 
 import aiohttp
 from sqlalchemy.exc import IntegrityError
 from telethon.tl.types import Message
 
-from config.settings import WEATHER_API_URL, WEATHER_API_KEY
+from config.settings import WEATHER_API_URL, WEATHER_API_KEY, CURRENCY_API_URL
 from models import NewsMessage, get_async_session
 
 logger = logging.getLogger(__name__)
 
 async_session = get_async_session()
 
-PRETTY_MESSAGE = '''**Погода в городе {city}**
+PRETTY_WEATHER_MESSAGE = '''**Погода в городе {city}**
 В течении дня:
 -- Cредняя температура  {mean_temp}°C
 -- Максимальная температура {max_temp}°C
 -- Минимальная температура {min_temp}°C'''
 
+PRETTY_RATE_MESSAGE = '''**Курс валют на сегодня**
+Доллар: {usd}
+Евро: {eur}
+'''
 
 async def create_news(message: Message):
     """
     Create news from message
     :param message: telethon.Message
-    :return:
     """
     async with async_session() as session:
         news = NewsMessage(text=message.message, created_at=message.date)
@@ -50,8 +54,22 @@ class Weather:
         max_temp = int(max([temp['main']['temp_max'] for temp in weather['list']]))
         mean_temp = int(sum([temp['main']['temp'] for temp in weather['list']]) / len(weather['list']))
 
-        return PRETTY_MESSAGE.format(city=city, mean_temp=mean_temp, max_temp=max_temp, min_temp=min_temp)
+        return PRETTY_WEATHER_MESSAGE.format(city=city, mean_temp=mean_temp, max_temp=max_temp, min_temp=min_temp)
 
 
+async def get_pretty_rate():
+    """
+    Get pretty rate
+    :return: str
+    """
+    rate = await _get_rate()
+    return PRETTY_RATE_MESSAGE.format(usd=rate['Valute']['USD']['Value'], eur=rate['Valute']['EUR']['Value'])
 
 
+async def _get_rate():
+    async with aiohttp.ClientSession() as session:
+        url = CURRENCY_API_URL
+        async with session.get(url) as resp:
+            data = await resp.read()
+            data_json = json.loads(data)
+            return data_json
--- src/config/db.py
+++ src/config/db.py
@@ -7,4 +7,4 @@ from .settings import DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT
 Base = declarative_base()
 connection_url = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'  # for alembic
 async_connection_url = f'postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-engine = create_async_engine(async_connection_url, echo=True)
+engine = create_async_engine(async_connection_url)
