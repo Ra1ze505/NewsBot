e7724417e833d53425d99d5a50c32eda655c0cf4
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-05-25 17:33:03 +0300

    new handlers

---
+++ src/app/bot/handlers.py
@@ -0,0 +1,45 @@
+from telethon import events
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+from app.bot.service import user_create, change_city, START_MESSAGE, change_time_mailing
+from app.parser.service import get_weather, get_pretty_rate
+from config.settings import BOT_TOKEN, API_ID, API_HASH
+from app.bot.buttons import start_markup
+
+bot = TelegramClient(StringSession(), API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
+
+@bot.on(events.NewMessage(pattern='/start'))
+async def start_handler(event):
+    await user_create(event.sender_id)
+    await event.respond(START_MESSAGE, buttons=start_markup)
+
+
+@bot.on(events.NewMessage(pattern='Изменить город'))
+async def change_city_handler(event):
+    async with bot.conversation(event.sender_id) as conv:
+        await change_city(conv)
+
+
+@bot.on(events.NewMessage(pattern='Изменить время рассылки'))
+async def change_time_mailing_handler(event):
+    async with bot.conversation(event.sender_id) as conv:
+        await change_time_mailing(conv)
+
+
+@bot.on(events.NewMessage(pattern='Погода'))
+async def weather_handler(event):
+    weather_message = await get_weather(event.sender_id)
+    await event.respond(weather_message)
+
+
+@bot.on(events.NewMessage(pattern='Курс'))
+async def rate_handler(event):
+    rate_message = await get_pretty_rate()
+    await event.respond(rate_message)
+
+
+# For run bot
+with bot:
+    bot.run_until_disconnected()
