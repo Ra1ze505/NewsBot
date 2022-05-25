5e09ef129c8c13dfc74cf494ca791b251d02affe
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-05-25 17:33:23 +0300

    update buttons

--- src/app/bot/buttons.py
+++ src/app/bot/buttons.py
@@ -1,10 +1,11 @@
 from telethon import Button
 
 
-start_markup = [Button.text('Погода'), Button.text('Курс'), Button.text('Изменить город'), Button.text('Изменить время рассылки')]
+start_markup = [Button.text('Погода', resize=True), Button.text('Курс'),
+                Button.text('Изменить город'), Button.text('Изменить время рассылки')]
 
 cansel_markup = [Button.text('Отмена')]
 
-change_time_markup = [Button.text('8:00'), Button.text('10:00'), Button.text('12:00'), Button.text('Отмена')]
+change_time_markup = [Button.text('8:00', resize=True), Button.text('10:00'), Button.text('12:00'), Button.text('Отмена')]
 
-change_city_markup = [Button.text('Москва'), Button.text('Санкт-Петербург'), Button.text('Отмена')]
+change_city_markup = [Button.text('Москва', resize=True), Button.text('Санкт-Петербург'), Button.text('Отмена')]
