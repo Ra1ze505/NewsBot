90edb170932600727046f533cc1332deb59f8826
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-05-28 15:10:28 +0300

    add type hinting

--- src/app/bot/handlers.py
+++ src/app/bot/handlers.py
@@ -2,9 +2,9 @@ from telethon import events
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
-from app.bot.service import user_create, change_city, START_MESSAGE, change_time_mailing
+from app.bot.service import user_create, change_city, START_MESSAGE, change_time_mailing, get_user_city
 from app.mailing.service import get_day_news
-from app.parser.service import get_weather, get_pretty_rate
+from app.parser.service import WeatherService, get_pretty_rate
 from config.settings import BOT_TOKEN, API_ID, API_HASH
 from app.bot.buttons import start_markup
 
@@ -12,37 +12,38 @@ bot = TelegramClient(StringSession(), API_ID, API_HASH).start(bot_token=BOT_TOKE
 
 
 @bot.on(events.NewMessage(pattern='/start'))
-async def start_handler(event):
+async def start_handler(event: events.NewMessage.Event):
     await user_create(event.sender_id)
     await event.respond(START_MESSAGE, buttons=start_markup)
 
 
 @bot.on(events.NewMessage(pattern=r'Изменить\sгород$'))
-async def change_city_handler(event):
+async def change_city_handler(event: events.NewMessage.Event):
     async with bot.conversation(event.sender_id) as conv:
         await change_city(conv)
 
 
 @bot.on(events.NewMessage(pattern=r'Изменить\sвремя\sрассылки$'))
-async def change_time_mailing_handler(event):
+async def change_time_mailing_handler(event: events.NewMessage.Event):
     async with bot.conversation(event.sender_id) as conv:
         await change_time_mailing(conv)
 
 
 @bot.on(events.NewMessage(pattern=r'Погода$'))
-async def weather_handler(event):
-    weather_message = await get_weather(event.sender_id)
+async def weather_handler(event: events.NewMessage.Event):
+    user_city = await get_user_city(event.sender_id)
+    weather_message = await WeatherService().get_pretty_weather(user_city)
     await event.respond(weather_message, buttons=start_markup)
 
 
 @bot.on(events.NewMessage(pattern=r'Курс$'))
-async def rate_handler(event):
+async def rate_handler(event: events.NewMessage.Event):
     rate_message = await get_pretty_rate()
     await event.respond(rate_message, buttons=start_markup)
 
 
 @bot.on(events.NewMessage(pattern=r'Новости$'))
-async def help_handler(event):
+async def help_handler(event: events.NewMessage.Event):
     news = await get_day_news()
     await event.respond(news.text, buttons=start_markup)
 
