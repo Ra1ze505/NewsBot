ad9b2a4d38964d27f5cf357727d8fcb4a0b2606b
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-05-28 15:11:48 +0300

    add type hinting

--- src/app/bot/service.py
+++ src/app/bot/service.py
@@ -26,28 +26,28 @@ async def commit(session: AsyncSession):
         await session.rollback()
 
 
-async def user_create(chat_id: int):
+async def user_create(chat_id: int) -> None:
     async with async_session() as session:
         user = User(chat_id=chat_id)
         session.add(user)
         await commit(session)
 
 
-async def get_user_city(chat_id: int):
+async def get_user_city(chat_id: int) -> str:
     async with async_session() as session:
         stmt = select([User.city]).where(User.chat_id == chat_id)
         user = await session.execute(stmt)
         return user.fetchone().city
 
 
-async def get_time_mailing(chat_id: int):
+async def get_time_mailing(chat_id: int) -> datetime.time:
     async with async_session() as session:
         stmt = select([User.time_mailing]).where(User.chat_id == chat_id)
         user = await session.execute(stmt)
         return user.fetchone().time_mailing
 
 
-async def valid_city(city: str):
+async def valid_city(city: str) -> (bool, int):
     async with aiohttp.ClientSession() as session:
         async with session.get(WEATHER_API_NOW_URL.format(city=city, token=WEATHER_API_KEY)) as resp:
             if resp.status == 200:
@@ -56,21 +56,21 @@ async def valid_city(city: str):
             return False, None
 
 
-async def user_city_update(chat_id: int, city: str, timezone: int):
+async def user_city_update(chat_id: int, city: str, timezone: int) -> None:
     async with async_session() as session:
         stmt = User.__table__.update().where(User.chat_id == chat_id).values(city=city, timezone=timezone)
         await session.execute(stmt)
         await commit(session)
 
 
-async def user_time_mailing_update(chat_id: int, time_mailing: datetime.time):
+async def user_time_mailing_update(chat_id: int, time_mailing: datetime.time) -> None:
     async with async_session() as session:
         stmt = User.__table__.update().where(User.chat_id == chat_id).values(time_mailing=time_mailing)
         await session.execute(stmt)
         await commit(session)
 
 
-async def change_city(conv):
+async def change_city(conv) -> None:
     city = await get_user_city(conv.chat_id)
     await conv.send_message(f'Ваш город сейчас: {city}\nНапишите свой город', buttons=change_city_markup)
     new_city, timezone, updated = await _get_city(conv, city)
@@ -81,7 +81,7 @@ async def change_city(conv):
         await conv.send_message('Ваш город не изменен', buttons=start_markup)
 
 
-async def _get_city(conv, city: str = None):
+async def _get_city(conv, city: str = None) -> (str, int, bool):
     answer = await conv.get_response()
     if answer.text == 'Отмена':
         return city, None, False
@@ -93,7 +93,7 @@ async def _get_city(conv, city: str = None):
         return await _get_city(conv)
 
 
-async def change_time_mailing(conv):
+async def change_time_mailing(conv) -> None:
     time_mailing: datetime.time = await get_time_mailing(conv.chat_id)
     await conv.send_message(f'Ваше время получения рассылки сейчас: {time_mailing.strftime("%H:%M")}\nОтправьте новое '
                             f'время получения рассылки\nP.S. вы можете указать любое время☺️',
@@ -108,7 +108,7 @@ async def change_time_mailing(conv):
         await conv.send_message('Ваше время получения рассылки не изменено', buttons=start_markup)
 
 
-async def _get_time_mailing(conv, time_mailing: str = None):
+async def _get_time_mailing(conv, time_mailing: str = None) -> (datetime.time, bool):
     answer = await conv.get_response()
     if answer.text == 'Отмена':
         return time_mailing, False
--- src/tasks/service.py
+++ src/tasks/service.py
@@ -3,7 +3,7 @@ from sqlalchemy import select
 from models import User, get_async_session
 
 
-async def get_users():
+async def get_users() -> list[User]:
     async_session = get_async_session()
     async with async_session() as session:
         stmt = select([User.chat_id, User.timezone, User.time_mailing])
